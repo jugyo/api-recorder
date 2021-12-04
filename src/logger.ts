4e170e053e0f34874692d7943718e76be3854b94
jugyo <kkohno@gmail.com>
2021-12-03 21:04:29 -0500

    Better log output

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,16 @@
 export default function (req, res, next) {
   res.on("finish", () => {
+    let authHeader = "";
+    try {
+      authHeader = req.headers.authorization
+        .replace(/Bearer\s+/, "")
+        .slice(0, 20);
+    } catch (error) {}
+
     console.log(
-      `${req.method} ${req.originalUrl} ${res.statusCode}${
-        res._cacheHit ? " (CACHE HIT)" : ""
-      }`
+      `${authHeader ? authHeader + "... " : ""}${req.method} ${
+        req.originalUrl
+      } ${res.statusCode}${res._cacheHit ? " (CACHE HIT)" : ""}`
     );
   });
 
